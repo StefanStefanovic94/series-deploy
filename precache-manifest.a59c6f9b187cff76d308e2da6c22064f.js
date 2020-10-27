b088312b8dd68006b2e29ed154e7983f497b7d34
stefanstefanovic94 <stefan.stef94@gmail.com>
2020-10-28 00:09:11 +0100

    Updates

---
+++ precache-manifest.a59c6f9b187cff76d308e2da6c22064f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccea37d8b1b649ed0cda72540e947506",
+    "url": "/series-deploy/index.html"
+  },
+  {
+    "revision": "d36862aa004a43df4996",
+    "url": "/series-deploy/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "1c15fb37c1b7d80618f9",
+    "url": "/series-deploy/static/css/main.1da29936.chunk.css"
+  },
+  {
+    "revision": "d36862aa004a43df4996",
+    "url": "/series-deploy/static/js/2.12cd6c35.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/series-deploy/static/js/2.12cd6c35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c15fb37c1b7d80618f9",
+    "url": "/series-deploy/static/js/main.f5dc966d.chunk.js"
+  },
+  {
+    "revision": "496d81fb5003206738bd",
+    "url": "/series-deploy/static/js/runtime-main.9635bd92.js"
+  }
+]);
